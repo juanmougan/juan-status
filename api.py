50dc8cb4e29200c0927e87d4a52a75958087c757
Juan Manuel Mougan <juanmougan@gmail.com>
2019-05-26 22:25:24 -0300

    Updated

--- api.py
+++ api.py
@@ -11,17 +11,22 @@ def projects():
 def current_projects():
 	p1 = {
 		'priority' : 1,
-		'description' : 'Reading about Android',
-		'topics' : ['mobile', 'Android', 'research']
+		'description' : 'A web scraper example using Python',
+		'topics' : ['webscraping', 'Python']
 	}
 	p2 = {
 		'priority' : 2,
-		'description' : 'Create a separate frontend for QuePido app',
-		'topics' : ['web-development', 'frontend', 'React']
+		'description' : 'Reading about Android',
+		'topics' : ['mobile', 'Android', 'research']
 	}
 	p3 = {
 		'priority' : 3,
+		'description' : 'Create a separate frontend for QuePido app',
+		'topics' : ['web-development', 'frontend', 'React']
+	}
+	p4 = {
+		'priority' : 4,
 		'description' : 'Some random Ruby stuff (and even Crystal)',
 		'topics' : ['web-development', 'backend', 'Ruby', 'Crystal']
 	}
-	return [p1, p2, p3]
+	return [p1, p2, p3, p4]
